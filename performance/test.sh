8433fcdd95abb8e8f05e7f895991d513bd024a9b
Florian Mantz <fmantz@gmx.de>
2020-11-21 15:26:47 +0100

    performance measures to csv

--- performance/test.sh
+++ performance/test.sh
@@ -3,7 +3,7 @@
 export LEVEL=3
 export NEWDIR="$(date '+%Y-%m-%d_%H-%M-%S').logs"
 
-mkdir ./performance/$NEWDIR
+mkdir /root/performance/$NEWDIR
 
 let NUMBER_OF_PUZZLES=1
 
@@ -15,21 +15,24 @@ echo LEVEL = $c   PUZZLES = $NUMBER_OF_PUZZLES
 echo *****************************************
 
 #Generate sudokus with QQwing:
-/usr/bin/time -v -a --output=./performance/$NEWDIR/qqwing_gen.log -p sh -c "java -jar /root/qqwing-1.3.4.jar --generate ${NUMBER_OF_PUZZLES} --difficulty expert --compact > ./performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}.txt"
+/usr/bin/time -v -a --output=/root/performance/$NEWDIR/qqwing_gen.log -p sh -c "java -jar /root/qqwing-1.3.4.jar --generate ${NUMBER_OF_PUZZLES} --difficulty expert --compact > /root/performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}.txt"
 
 #Solve sudokus with QQwing (reference):
-/usr/bin/time -v -a --output=./performance/$NEWDIR/sudoku-qqwing.log -p sh -c "cat ./performance/$NEWDIR/sudoku${NUMBER_OF_PUZZLES}.txt | java -jar /root/qqwing-1.3.4.jar --solve --compact > ./performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}_sol-qqwing.txt"
+/usr/bin/time -v -a --output=/root/performance/$NEWDIR/sudoku-qqwing.log -p sh -c "cat /root/performance/$NEWDIR/sudoku${NUMBER_OF_PUZZLES}.txt | java -jar /root/qqwing-1.3.4.jar --solve --compact > /root/performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}_sol-qqwing.txt"
 
 #Solve sudokus with rust sudoku:
-/usr/bin/time -v -a --output=./performance/$NEWDIR/sudoku-rust.log -p sh -c "/root/sudoku-rust ./performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}.txt ./performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}_sol-rust.txt"
+/usr/bin/time -v -a --output=/root/performance/$NEWDIR/sudoku-rust.log -p sh -c "/root/sudoku-rust /root/performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}.txt /root/performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}_sol-rust.txt"
 
 #Solve sudokus with scala sudoku:
-/usr/bin/time -v -a --output=./performance/$NEWDIR/sudoku-scala.log -p sh -c "java -jar /root/sudoku-scala.jar ./performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}.txt ./performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}_sol-scala.txt"
+/usr/bin/time -v -a --output=/root/performance/$NEWDIR/sudoku-scala.log -p sh -c "java -jar /root/sudoku-scala.jar /root/performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}.txt /root/performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}_sol-scala.txt"
 
 #Solve sudokus with scalanative sudoku:
-/usr/bin/time -v -a --output=./performance/$NEWDIR/sudoku-scalanative.log -p sh -c "/root/sudoku-scalanative ./performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}.txt ./performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}_sol-scalanative.txt"
+/usr/bin/time -v -a --output=/root/performance/$NEWDIR/sudoku-scalanative.log -p sh -c "/root/sudoku-scalanative /root/performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}.txt /root/performance/${NEWDIR}/sudoku${NUMBER_OF_PUZZLES}_sol-scalanative.txt"
 
 let NUMBER_OF_PUZZLES=$(($NUMBER_OF_PUZZLES*10))
 
 done
+
+scala /root/performance/prepare_data.sh /root/performance/${NEWDIR}
+
 exit
