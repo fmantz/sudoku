8284f8d752043c5a5992269d5ffa138c45707ded
Florian Mantz <fmantz@gmx.de>
2020-11-22 13:02:45 +0100

    bugfixes

--- performance/prepare_data.sh
+++ performance/prepare_data.sh
@@ -2,25 +2,27 @@
 exec scala "$0" "$@"
 !#
 
-#
-# sudoku - Sudoku solver for comparison Scala with Rust
-#        - The motivation is explained in the README.md file in the top level folder.
-# Copyright (C) 2020 Florian Mantz
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
 
+//scalastyle:off
+/*
+ * sudoku - Sudoku solver for comparison Scala with Rust
+ *        - The motivation is explained in the README.md file in the top level folder.
+ * Copyright (C) 2020 Florian Mantz
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+//scalastyle:on
 object PrepareData {
 
   import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
@@ -43,7 +45,7 @@ object PrepareData {
 				name.startsWith("sudoku") && name.endsWith("log")
 			})
 
-		val rsData = mutable.Map.eprintlmpty[String, (Array[String], Array[String])]
+		val rsData = mutable.Map.empty[String, (Array[String], Array[String])]
 
 		//Extract data:
 		for(file <- filesToConsider){
