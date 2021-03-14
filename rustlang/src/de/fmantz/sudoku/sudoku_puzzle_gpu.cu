1b687c13e4578ffbea6cb1b01a95ad422cbe8593
Florian Mantz <fmantz@gmx.de>
2021-03-14 17:57:38 +0100

    first cuda code added.

---
+++ rustlang/src/de/fmantz/sudoku/sudoku_puzzle_gpu.cu
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+__global__ void cuda_hello(){
+    printf("Hello World from GPU! %d\n", threadIdx.x*gridDim.x);
+}
+
+//simple dummy hello world:
+int main(){
+    printf("Hello World from CPU!\n");
+//    cuda_hello<<<500,1024>>>();
+    cuda_hello<<<1,10>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
