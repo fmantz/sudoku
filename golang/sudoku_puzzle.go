19343a64f7ae965eb113a3f956bb01af494ba2cd
Florian Mantz <fmantz@gmx.de>
2023-01-12 19:52:27 +0100

    add some golang code

--- golang/sudoku_puzzle.go
+++ golang/sudoku_puzzle.go
@@ -35,12 +35,64 @@ func (p *SudokuPuzzle) Solve() bool {
 	return false //TODO
 }
 
-func (p *SudokuPuzzle) findAllPossibleValuesForEachEmptyCell(row, col int, value uint8) {
+func (p *SudokuPuzzle) findAllPossibleValuesForEachEmptyCell(rowNums, colNums, squareNums []uint16) {
 	for i := 0; i < CELL_COUNT; i++ {
 		curValue := p.puzzle[i]
 		if curValue > 0 {
-			// TODO
+			p.saveValueForCellAndCheckIsSolvable(curValue, i, rowNums, colNums, squareNums)
 		}
+	}
+}
+
+func (p *SudokuPuzzle) sortPuzzle(puzzleSorted, indices []uint8) {
+	for i := 0; i < CELL_COUNT; i++ {
+		puzzleSorted[i] = p.puzzle[indices[i]]
+	}
+}
+
+func (p *SudokuPuzzle) fillPositions(puzzleSorted, indices []uint8) {
+	for i := 0; i < CELL_COUNT; i++ {
+		p.puzzle[indices[i]] = puzzleSorted[i]
+	}
+}
+
+func (p *SudokuPuzzle) saveValueForCellAndCheckIsSolvable(value uint8, index int, rowNums, colNums, squareNums []uint16) {
+	rowIndex := calculateRowIndex(index)
+	colIndex := calculateColIndex(index)
+	squareIndex := calculateSquareIndex(rowIndex, colIndex)
+	checkBit := uint16(1) << (value - 1)
+	p.myIsSolvable = p.myIsSolvable && setAndCheckBit(checkBit, rowNums, rowIndex)
+	p.myIsSolvable = p.myIsSolvable && setAndCheckBit(checkBit, colNums, colIndex)
+	p.myIsSolvable = p.myIsSolvable && setAndCheckBit(checkBit, squareNums, squareIndex)
+}
+
+func setAndCheckBit(checkBit uint16, array []uint16, index int) bool {
+	oldValue := array[index]
+	array[index] |= checkBit
+	return oldValue != array[index]
+}
+
+func calculateRowIndex(index int) int {
+	return index / PUZZLE_SIZE
+}
+
+func calculateColIndex(index int) int {
+	return index % PUZZLE_SIZE
+}
+
+func calculateSquareIndex(rowIndex, colIndex int) int {
+	return rowIndex/SQUARE_SIZE*SQUARE_SIZE + colIndex/SQUARE_SIZE //attention: int arithmetic
+}
+
+func (p *SudokuPuzzle) getPossibleCounts(index int, rowNums, colNums, squareNums []uint16) uint8 {
+	if p.puzzle[index] == 0 {
+		rowIndex := calculateRowIndex(index)
+		colIndex := calculateColIndex(index)
+		squareIndex := calculateSquareIndex(rowIndex, colIndex)
+		possibleNumberIndex := rowNums[rowIndex] | colNums[colIndex] | squareNums[squareIndex]
+		return BITSET_LENGTH[possibleNumberIndex]
+	} else {
+		return 0
 	}
 }
 
